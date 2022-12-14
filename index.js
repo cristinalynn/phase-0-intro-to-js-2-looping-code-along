6a18da81e690cf98ff07d9a145a28fbbb81cae29
Cristina Lynn <cristina@cristinalynn.net>
2022-12-13 19:40:57 -0800

    commit tree 0ac307150ea1d04b5eff34fe56f18f6cf9d4251a

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(array, event) {
+    let completedCards = []
+    for(let i = 0; i < array.length; i++) {
+        
+        completedCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards);
+    }
+    return completedCards;
+}
+
+writeCards(names, "Suprise")
+
+function countDown(i) {
+    while (i > 0) {
+        console.log(i--);
+    } 
+    console.log(i)
+}
+countDown(10)
